f2758a8aee455dbd508e2e52ba5071c564f66fa5
Artiom_Sokolov <sokolova-net@mail.ru>
2023-05-08 20:22:44 +0300

    Update 2023-05-08T17:22:32.273Z

--- src/js/_components.js
+++
@@ -1,6 +0,0 @@
-import './components/input-dest';
-import './components/ex';
-import './components/easepick';
-import './components/subscribe-label';
-import './components/mobile-nav';
-import './components/btn-to-top';
--- src/js/_vendor.js
+++
@@ -1,4 +0,0 @@
-import './vendor/focus-visible.js';
-import './vendor/autoComplete/autoComplete.min';
-import './vendor/custom-select.min';
-import './vendor/easepick.min';
